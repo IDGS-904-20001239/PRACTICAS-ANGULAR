8bc659b3815bcf3ed4f69f1b56389b68170be70b
yair <yair.teran47@gmail.com>
2023-07-04 23:08:48 -0600

    EYET 04/07/2023

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,14 @@ import { DistanciaModule } from './distancia/distancia/distancia.module';
 import { ResistenciasModule } from './resistencias/resistencia/resistencias.module';
 import { CinepolisModule } from './cinepolis/cinepolis.module';
 import { AppRoutingModule } from './app-routing.module';
+import { PizzasComponent } from './formularios/pizzas/pizzas.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    MenuComponent,
+    PizzasComponent,
   ],
   imports: [
     DistanciaModule,
